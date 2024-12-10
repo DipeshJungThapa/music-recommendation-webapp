7b7a15effee677e45bdbaf6b23a21c5b66cecc0d
binayakjoshi <185522100+binayakjoshi@users.noreply.github.com>
2024-12-10 17:09:51 +0545

    filled empty cell of col: track genre_top

--- ml-model/clean.py
+++ ml-model/clean.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 merged = pd.read_csv("./merged_data.csv")
-
-print(merged.columns)
-
+#fill empty cells with 'Unknown'
+merged['track genre_top'] = merged['track genre_top'].fillna('Unknown')
+merged.to_csv("merged_and_cleaned.csv", index=False)
+print("Data has been successfully cleaned and saved as 'merged_and_cleaned.csv'.")
+#fill empty numerical cells with mean or median 
