316feeaad9c68ebf2c0b835fd06389e0668e79ca
binayakjoshi <185522100+binayakjoshi@users.noreply.github.com>
2025-02-11 20:55:10 +0545

    controller function

---
+++ server/controllers.js
@@ -0,0 +1,50 @@
+const fileUpload = require('./file-upload');
+const path = require('path');
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
+
+const recommendation = async (req, res) => {
+  const audioFile = req.file;
+  if (!audioFile) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const filePath = path.join(__dirname, audioFile.path);
+
+  const form = new FormData();
+  form.append('file', fs.createReadStream(filePath), {
+    filename: audioFile.originalname,
+    contentType: audioFile.mimetype,
+  });
+
+  try {
+    const response = await axios.post('http://localhost:7000/recommend', form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    });
+    console.log('Response from recommendation service:', response.data);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting file:', err);
+      }
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(
+      'Error sending file to recommendation service:',
+      error.response?.data || error.message
+    );
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting file after failure:', err);
+      }
+    });
+
+    res.status(500).json({ error: 'Failed to process the file' });
+  }
+};
+
+exports.recommendation = recommendation;
--- server/routes.js
+++ server/routes.js
@@ -1,53 +1,13 @@
 const express = require('express');
+const controller = require('./controllers');
 const fileUpload = require('./file-upload');
-const path = require('path');
-const fs = require('fs');
-const axios = require('axios');
-const FormData = require('form-data');
 
 const router = express.Router();
 
-router.post('/upload', fileUpload.single('audioFile'), async (req, res) => {
-  const audioFile = req.file;
-  if (!audioFile) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  }
-
-  const filePath = path.join(__dirname, audioFile.path);
-
-  const form = new FormData();
-  form.append('file', fs.createReadStream(filePath), {
-    filename: audioFile.originalname,
-    contentType: audioFile.mimetype,
-  });
-
-  try {
-    const response = await axios.post('http://localhost:7000/recommend', form, {
-      headers: {
-        ...form.getHeaders(),
-      },
-    });
-    console.log('Response from recommendation service:', response.data);
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error('Error deleting file:', err);
-      }
-    });
-
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.error(
-      'Error sending file to recommendation service:',
-      error.response?.data || error.message
-    );
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error('Error deleting file after failure:', err);
-      }
-    });
-
-    res.status(500).json({ error: 'Failed to process the file' });
-  }
-});
+router.post(
+  '/upload',
+  fileUpload.single('audioFile'),
+  controller.recommendation
+);
 
 module.exports = router;
